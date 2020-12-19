2f96cd90b9f9d3654f5b53fb0fa5c8d2f4c3f13a
Ch1ch1 <fred.chinchin@gmail.com>
2020-12-19 23:38:50 +0100

    Alpine 3:12

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM alpine
+FROM alpine:3.12
 RUN apk update && \
     apk upgrade -U && \
     apk add bash ca-certificates ffmpeg libwebp libwebp-tools && \
     rm -rf /var/cache/*
-COPY ./convert.sh /
+COPY convert.sh /
 RUN chmod +x /convert.sh
+
 ENTRYPOINT ["/convert.sh"]
+CMD ["sh"]
--- convert.sh
+++ convert.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-PARAMS=('-m 6 -q 70 -mt -af -progress')
-DIRECTORY=/drone/src/public/img/gallery
+PARAMS=('-m 6 -q 70 -mt -af -progress -resize 300 0 -low_memory')
+DIRECTORY="www/public/img/gallery"
+STRING="_thumb"
 
 if [ $# -ne 0 ]; then
 	PARAMS=$@;
 fi
-
 cd $DIRECTORY
 
-shopt -s globstar nullglob nocaseglob extglob
+shopt -s nullglob nocaseglob extglob
 
-for FILE in */*.@(jpg|jpeg|tif|tiff|png); do
-    cwebp $PARAMS "$FILE" -o "${FILE%.*}"_thumb.webp;
+for FILE in */*@(webp)
+do
+	if  [[ ! "$FILE" == *"$STRING"* ]]
+	 then
+		 if [[ -f $FILE ]] && [[ -f "${FILE%.*}"_thumb.webp ]]
+	 	 then
+	 		echo "Skiping $FILE file..."
+	 		continue
+		fi
+			 cwebp $PARAMS "$FILE" -o "${FILE%.*}"_thumb.webp
+	fi
+		 continue
 done
